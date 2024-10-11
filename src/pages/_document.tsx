f3dca0871eaa94f7716618e36111c42788d1fe07
Gabriel Rosa <git@secondmind.com.br>
2024-10-11 00:36:04 -0300

    #feat- main

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,8 @@ export default function Document() {
         <meta property="og:title" content="BlackValleyPerformance - Oficina Especializada em congonhas" />
         <meta name="description" content="A BlackValley é uma oficina especializada em manutenção de veículos, oferecendo serviços completos para garantir a melhor performance e durabilidade do seu carro. Além de realizar revisões, reparos mecânicos e elétricos, a oficina é referência no uso do VCDS, uma ferramenta de diagnóstico avançada utilizada principalmente em veículos da marca Volkswagen. Com ela, os profissionais da BlackValley conseguem identificar e resolver problemas eletrônicos com precisão, garantindo um serviço eficiente e personalizado para cada tipo de veículo." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <meta property="og:url" content="https://team-dardanete-homo.vercel.app/" />
-        <meta property="og:image" content="https://team-dardanete-homo.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlogoComPersonalBranco.1aefd167.png&w=384&q=75" /> */}
+        <meta property="og:url" content="https://black-valley.vercel.app/" />
+        <meta property="og:image" content="https://black-valley.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlogoBlackValley.ef4248fa.png&w=640&q=75" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:description" content="A BlackValley é uma oficina especializada em manutenção de veículos, oferecendo serviços completos para garantir a melhor performance e durabilidade do seu carro. Além de realizar revisões, reparos mecânicos e elétricos, a oficina é referência no uso do VCDS, uma ferramenta de diagnóstico avançada utilizada principalmente em veículos da marca Volkswagen. Com ela, os profissionais da BlackValley conseguem identificar e resolver problemas eletrônicos com precisão, garantindo um serviço eficiente e personalizado para cada tipo de veículo." />
